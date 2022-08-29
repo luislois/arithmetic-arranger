5daa0b069c10e86ce0e497bedff41bc7bd41e306
Luis Lois <76075356+luislois@users.noreply.github.com>
2022-08-29 11:30:49 +0000

    Add files via upload

---
+++ arithmetic_arranger.py
@@ -0,0 +1,58 @@
+import re
+
+def arithmetic_arranger(problems, solve = False):
+
+  if(len(problems) >5):
+    return "Error: to many problems"
+  
+  first=""
+  second=""
+  lines=""
+  sumx=""
+  string=""
+
+  for problem in problems:
+    if(re.search("[^\s0-9. +-]", problem)):
+      if(re.search("[/]", problem) or re.search("[+]", problem)):
+        return "Error: Operator must be '+' or '-'."
+    return "Error: Numbers must only contain digits."
+
+    firstNumber = problem.split(" ")[0]  
+    operator = problem.split(" ")[1]
+    secondNumber = problem.split(" ")[2]
+
+    if(len(firstNumber) >=5 or len(secondNumber) >=5):
+      return "Error: Numbers cannot be more than 4 digits"
+
+      sum = ""
+      if(operator == "+"):
+        sum= str(int(firstNumber) + int(secondNumber))
+      elif(operator == "-"):
+        sum= str(int(firstNumber) - int(secondNumber)) 
+
+    length = max(len(firstNumber), len(secondNumber)) + 2
+    top = str(firstNumber).rjust(length)
+    bottom = operator + str(secondNumber).rjust(length-1)
+    line=""
+    res= str(sum).rjust(length)
+    
+    for s in range (length)
+    line+=""
+
+    if problem != problem(-1)
+      first+= top +'    '
+      second+= bottom +'    '
+      lines+= line +'    '
+      sumx+= res +'    '
+    else:
+      first+= top
+      second+= bottom
+      ines+= line
+      sumx+= res
+
+  if solve:
+    string = first +"\n" + second +"\n" + lines + "\n" + sumx
+  else:
+    string = first +"\n" + second +"\n" + lines
+  return string  
+      
